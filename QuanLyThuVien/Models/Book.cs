5ed97b20252477bfa1147f3d4f2c35095c6a2cbe
khanhlq <khanh96le@gmail.com>
2017-04-26 07:58:42 +0700

    add model book

---
+++ QuanLyThuVien/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyThuVien.Models {
+    public class Book {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Name is required.")]
+        public string Name { get; set; }
+
+        public decimal Intro { get; set; }
+
+        public decimal Prize { get; set; }
+    }
+}
